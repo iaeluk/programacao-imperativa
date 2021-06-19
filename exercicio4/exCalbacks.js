edd291ed61548eed11fe4d6d28dc936456070c13
Lucas Mendonça <lucas21rio@gmail.com>
2021-06-18 22:23:46 -0300

    Melhor entendimento de callbacks

--- exercicio4/exCalbacks.js
+++ exercicio4/exCalbacks.js
@@ -1,24 +1,18 @@
-let acaoCarro = (callback) => {
-    console.log("O carro estás " + callback)
-}
-
-let andar = () => {
-    return "andando."
-}
-
-let parar = () => {
-    return "parado."
-}
-
-acaoCarro(andar())
-acaoCarro(parar())
-
-
 /*
 Crie uma função acaoCarro(), que receberá uma callback como parâmetro. Após isso, crie duas funções: andar() – deve exibir uma mensagem dizendo que o carro está andando – e parar() – deve exibir uma mensagem dizendo que o carro parou.
 
 Execute ambas as ações utilizando a função acaoCarro(), passando suas ações como callbacks.
 
-
-
 */ 
+
+let acaoCarro = (callback) => {
+    console.log(`O carro está ${callback()}`)
+}
+
+let andar = () => "andando.";
+
+let parar = () => "parado.";
+
+acaoCarro(andar)
+acaoCarro(parar)
+
