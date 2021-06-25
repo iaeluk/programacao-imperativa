a7edccd4fa2ad92a49c9f363813984d32965b47a
Lucas Mendonça <lucas21rio@gmail.com>
2021-06-25 02:05:31 -0300

    Exercício de módulos

--- exercicio6/app.js
+++ exercicio6/app.js
@@ -1,16 +1,16 @@
 const [somar, subtrair, dividir, multiplicar] = require('./calcular/calcular');
 
 
-let soma = somar(100, 2) //5
+let soma = somar(100, 2)
 console.log(soma);
 
-let subtracao = subtrair(77, 3); //5
+let subtracao = subtrair(77, 3);
 console.log(subtracao);
 
-let divisao = dividir(50, 4); //25
+let divisao = dividir(50, 4);
 console.log(divisao);
 
-let multiplicacao = multiplicar(8, 25); //75
+let multiplicacao = multiplicar(8, 25);
 console.log(multiplicacao);
 
 
