8b2137992bc595bad1e75264c908de985776c382
Lucas Mendonça <lucas21rio@gmail.com>
2021-06-29 12:20:21 -0300

    Importando módulos com npm

---
+++ exercicio7/app.js
@@ -0,0 +1,5 @@
+const fatorial = require('fatorial'); // módulo que calcula o fatorial.
+const fibo = require('fibonacci_sequence/lib'); // módulo que calcula a sequência de fibonacci.
+
+console.log(fatorial.calcula(8));
+console.log(fibo.fibonacciSequence(10));
