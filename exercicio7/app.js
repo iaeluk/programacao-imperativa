58e28f82917403153950da5e5dce406b15c6b3ad
Lucas Mendonça <lucas21rio@gmail.com>
2021-06-29 16:47:59 -0300

    Novo caminho no require.

--- exercicio7/app.js
+++ exercicio7/app.js
@@ -1,5 +1,5 @@
-const fatorial = require('fatorial'); // módulo que calcula o fatorial.
-const fibo = require('fibonacci_sequence/lib'); // módulo que calcula a sequência de fibonacci.
+const fatorial = require('./node_modules/fatorial/fatorial'); // módulo que calcula o fatorial.
+const fibo = require('./node_modules/fibonacci_sequence/lib/index'); // módulo que calcula a sequência de fibonacci.
 
-console.log(fatorial.calcula(8));
+console.log(fatorial.calcula(5));
 console.log(fibo.fibonacciSequence(10));
